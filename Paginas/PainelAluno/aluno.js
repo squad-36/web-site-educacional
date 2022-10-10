44fd4aa76d11820112ade00dcd66387811562abf
Ricardo <ricardoaraujo3713@gmail.com>
2022-10-10 10:30:18 -0300

    local storage a desenvolver

--- Paginas/PainelAluno/aluno.js
+++ Paginas/PainelAluno/aluno.js
@@ -2,7 +2,7 @@ let bgAside = document.querySelector('aside');
 let bgHeader = document.querySelector('header');
 let btnTema = document.getElementById('btnTema');
 let capa = document.querySelector('.capa');
-let temaStorage = ""
+// let temaStorage = ""
 
 btnTema.addEventListener('click' , () => {
     bgAside.classList.toggle("bg-aluno");
@@ -18,28 +18,27 @@ btnTema.addEventListener('click' , () => {
 });
 
 
-function definesName() { 
+// function definesName() { 
    
-    
-    localStorage.setItem('Name', promptName);
-    let nameSaved = localStorage.getItem('Name');
+//     localStorage.setItem('Name', promptName);
+//     let nameSaved = localStorage.getItem('Name');
   
     
-    if(!localStorage.getItem('Name') || !promptName){
-        definesName();
-    }else{  
-        inputWriteName.innerHTML = 'Olá, Bem Vindo ' + nameSaved;
-        btnMyName.innerHTML = `Não é ${nameSaved}? Click!`
-    }
-}
+//     if(!localStorage.getItem('Name') || !promptName){
+//         definesName();
+//     }else{  
+//         inputWriteName.innerHTML = 'Olá, Bem Vindo ' + nameSaved;
+//         btnMyName.innerHTML = `Não é ${nameSaved}? Click!`
+//     }
+// }
 
-//Quando a pagina carregar Verificar se existe valor salvo no localstorage
+// //Quando a pagina carregar Verificar se existe valor salvo no localstorage
 
-window.addEventListener('load', function() {
-    let localStorageName = localStorage.getItem('Name');
-    if(localStorageName !== null){
-        inputWriteName.innerHTML = 'Olá, Bem Vindo ' + localStorageName;
-        btnMyName.innerHTML = `Não é ${localStorageName}? Click!`
-    }
-});
+// window.addEventListener('load', function() {
+//     let localStorageName = localStorage.getItem('Name');
+//     if(localStorageName !== null){
+//         inputWriteName.innerHTML = 'Olá, Bem Vindo ' + localStorageName;
+//         btnMyName.innerHTML = `Não é ${localStorageName}? Click!`
+//     }
+// });
 
