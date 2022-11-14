46798537ecc89d75cc289c666e2774946485bebd
Ricardo <ricardoaraujo3713@gmail.com>
2022-11-14 19:14:42 -0300

    Api Rest CSharp

---
+++ CsharpApi/CsharpApi/Models/Curso.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CsharpApi.Models
+{
+    [Table("Curso")]
+    public class Curso
+    {
+        [Key]
+        public int CursoId { get; set; }
+        [Required(ErrorMessage = "Informe a descrição do curso")]
+        [StringLength(50)]
+        public string Descricao { get; set; }
+        [Required(ErrorMessage = "Informe a carga horária")]
+        public int CargaHoraria { get; set; }
+    }
+}
---
+++ CsharpApi/CsharpApi/Models/CursoDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CsharpApi.Models;
+
+namespace CsharpApi.Models
+{
+    public class CursoDbContext : DbContext
+    {
+        public CursoDbContext(DbContextOptions<CursoDbContext> options)
+          : base(options)
+        { }
+
+        public DbSet<Curso> Cursos { get; set; }
+
+
+    }
+}
